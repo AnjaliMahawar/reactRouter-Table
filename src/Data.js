e4ebde87959a008babdfcad80011b84d9237d3fc
AnjaliMahawar <anjalimahawar011@gmail.com>
2022-09-22 18:37:29 +0530

    trying

--- src/Data.js
+++ src/Data.js
@@ -4,14 +4,37 @@ import Header from './Header'
 import React, { useRef } from 'react'
 
 export default function () {
-    const inputNameRef = useRef()
-    let getMyRow=(e)=>{
-        console.log('ok')
-     var c=  e.target.innerText;
-     console.log('c',c)
-     console.log("ref",inputNameRef.current)
-        
+    
+    const arr=[{
+
+        name:"Anjali",
+        Class:"Mca",
+        Address:"Neemuch",
+        Age:"18"
+    },
+    {
+        name:"kiran",
+        Class:"Mca",
+        Address:"parda",
+        Age:"25"
+    },
+    {
+        name:"Tannu",
+        Class:"Mca",
+        Address:"mds",
+        Age:"17"
+    },
+    {
+        name:"Anju",
+        Class:"Mca",
+        Address:"Indor",
+        Age:"20"
     }
+      ]
+      let getRow=(indx)=>{
+        console.log("get row")
+        console.log("indx",indx)
+      }
     return (
         <div>
             <Header></Header>
@@ -26,26 +49,21 @@ export default function () {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr onClick={getMyRow} ref={inputNameRef}>
-                            <td >1</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr onClick={getMyRow}ref={inputNameRef}>
-                            <td  >2</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr onClick={getMyRow} ref={inputNameRef}>
-                            <td >3</td>
-                            <td colSpan={2}>Larry the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                 {   arr.map((data, indx) =>
+                <tr onClick={()=>{getRow(indx)}} key={indx}
+                >
+                   
+                  <td>{data.name}</td>
+                  <td>{data.Class}</td>
+                  <td>{data.Address}</td>
+                  <td>{data.Age}</td>
+                  </tr>
+                 )
+                  }
                     </tbody>
                 </Table>
             
         </div>
     )
 }
+//https://stackoverflow.com/questions/37771316/react-triggering-click-event-on-table-row
